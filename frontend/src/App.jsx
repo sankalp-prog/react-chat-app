796144582a5a30051588c72e2178579b206dfd0b
Sankalp <planetsankalp@gmail.com>
2025-05-19 17:19:59 -0400

    Added daisyUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
 // NOTE: In the future you can use the react snippets extension - es7+ by dsznajder
 
 export default function App() {
-  return <h1 className="text-red-500">Hello World!</h1>;
+  return (
+    <>
+      <h1 className="text-red-500">Hello World</h1>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </>
+  );
 }
